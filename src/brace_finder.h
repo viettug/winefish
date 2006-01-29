4274160c2fb9a45c50bbd29f4e27bf6a18f69337
icy <kyanh@viettug.org>
2006-01-29 11:34:14 +0000

    support various retval and opt

--- src/brace_finder.h
+++ src/brace_finder.h
@@ -1,6 +1,21 @@
 #ifndef __BRACE_FINDER_H_
 #define _BRACE_FINDER_H_
 
-gboolean brace_finder(GtkTextBuffer *buffer, gboolean move_if_found);
+enum {
+BR_MOVE_IF_FOUND = 1<<0,
+BR_FIND_FORWARD = 1<<1,
+BR_FIND_BACKWARD = 1<<2
+};
+
+enum {
+BR_RET_FOUND,
+BR_RET_NOT_FOUND,
+BR_RET_IS_ESCAPED,
+BR_RET_IN_COMMENT,
+BR_RET_TOO_LONG,
+BR_RET_IN_SELECTION
+};
+
+gint brace_finder(GtkTextBuffer *buffer, gint opt);
 
 #endif /* __BRACE_FINDER_H_ */
