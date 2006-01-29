2c67261aad63d06c5ce1edfdbdc2239f3a61ace8
kyanh <devnull@localhost>
2006-01-29 11:35:02 +0000

    implement backward searching for }. fixed a bug: when a line is started by %, finder doesnot that this is a comment line

--- src/brace_finder.c
+++ src/brace_finder.c
@@ -21,12 +21,11 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#define DEBUG
+/* #define DEBUG */
 
 #include <gtk/gtk.h>
 
 #include "bluefish.h"
-#include "gui.h" /* statusbar_message() */
 #include "brace_finder.h"
 /* #include "document.h" */
 
@@ -62,8 +61,8 @@ static gboolean char_predicate(gunichar ch, gpointer data) {
 }
 
 /* kyanh, 20060128 */
-gboolean brace_finder(GtkTextBuffer *buffer, gboolean move_if_found) {
-	/* DEBUG_MSG("brace_finder: %%=%d,\\=%d,{=%d,}=%d,[=%d,]=%d\n", '%', '\\', '{','}','[', ']' ); */
+gint brace_finder(GtkTextBuffer *buffer, gint opt) {
+	DEBUG_MSG("\nbrace_finder: %%=%d,\\=%d,{=%d,}=%d,[=%d,]=%d, \\n=%d, \\r=%d \n", '%', '\\', '{','}','[', ']', '\n', '\r' );
 	GtkTextIter iter_start, iter_end;
 	GtkTextMark *insert, *select;
 	gboolean retval;
@@ -76,74 +75,102 @@ gboolean brace_finder(GtkTextBuffer *buffer, gboolean move_if_found) {
 
 	/* there's no selection */
 	if (gtk_text_iter_equal(&iter_start, &iter_end)){
-		GtkTextIter tmpiter;
+		GtkTextIter tmpiter, tmp2iter;
 		gint level;
 
 		/* A:: check if we are inside comment line */
 		tmpiter = iter_start;
 		gtk_text_iter_set_line_offset(&tmpiter,0); /* move to start of line */
 		/* now forward to find next %. limit = iter_start */
-		retval = gtk_text_iter_forward_find_char(&tmpiter, char_predicate, GINT_TO_POINTER(37), &iter_start);
-		if (retval && is_true_char(&tmpiter)) {
-			DEBUG_MSG("brace_finder: inside a comment line\n");
-			return FALSE;
+		ch = gtk_text_iter_get_char(&tmpiter);
+		if ( (ch == 37) || ( gtk_text_iter_forward_find_char(&tmpiter, char_predicate, GINT_TO_POINTER(37), &iter_start) && is_true_char(&tmpiter) ) ) {
+		/* if a line is started by percent, the gtk_text_iter_forward_find_char() still forwards to the next char ;) */
+			DEBUG_MSG("[found %%]");
+			return BR_RET_IN_COMMENT;
 		}
 		if (!is_true_char(&iter_start)) {
-			DEBUG_MSG("brace_finder: not a true {\n");
-			return FALSE;
+			DEBUG_MSG("[not a true { nor }]");
+			return BR_RET_IS_ESCAPED;
 		}
 
 		retval = FALSE;
 		ch = gtk_text_iter_get_char(&iter_start);
 
 		if (ch == 123 ) {/* { */
-		/* B:: now forward until '}' */
+			DEBUG_MSG("\nbrace_finder: find forward...=============\n");
 			tmpiter = iter_start; /* reset tmpiter */
 			level = 1;
 			/* we may meet } */
 			while (gtk_text_iter_forward_char(&tmpiter)) {
 				ch = gtk_text_iter_get_char(&tmpiter);
+				DEBUG_MSG("%c", ch);
 				/* DEBUG_MSG("brace_finder: enter new loop... with char = %c\n", ch); */
-				if (ch == 37) {/* % */
-					if (is_true_char(&tmpiter)) {
+				if ((ch == 37) && is_true_char(&tmpiter)) {/* % */
 #ifdef CANNOT_USE_THIS /* as we may skip the char followed the line end */
-						gtk_text_iter_forward_line(&tmpiter);
-						gunichar tmpch = gtk_text_iter_get_char(&tmpiter);
-						DEBUG_MSG("brace_finder: find true %%. forward to next line\n");
-						DEBUG_MSG("brace_finder: current char (after forwarding) %c\n", tmpch);
+					gtk_text_iter_forward_line(&tmpiter);
+					gunichar tmpch = gtk_text_iter_get_char(&tmpiter);
+					DEBUG_MSG("brace_finder: find true %%. forward to next line\n");
+					DEBUG_MSG("brace_finder: current char (after forwarding) %c\n", tmpch);
 #endif /* CANNOT_USE_THIS */
 					gtk_text_iter_forward_to_line_end(&tmpiter);
-					DEBUG_MSG("brace_finder: find true %%. forward to end of line\n");
+					DEBUG_MSG("[%%]");
+				}else if( (ch == 123)  && is_true_char(&tmpiter)) {/* { */
+					level ++;
+					DEBUG_MSG("[+%d]", level);
+				}else if((ch ==125) && is_true_char(&tmpiter)) {/* } */
+					level --;
+					DEBUG_MSG("[-%d]\n", level);
+					if (level==0) {
+						retval = TRUE;
+						break; /* finish the loop */
 					}
-				}else if(ch == 123) {/* { */
-					if (is_true_char(&tmpiter)) {
-						level ++;
-						DEBUG_MSG("brace_finder: find true {. level up to %d\n", level);
+				}
+			}
+		}else if (ch == 125) {/* } */
+			DEBUG_MSG("\nbrace_finder: find backward...=============\n");
+			level=1;
+			tmpiter = iter_start;
+			while (gtk_text_iter_backward_char(&tmpiter) ){
+				ch = gtk_text_iter_get_char(&tmpiter);
+				DEBUG_MSG("%c", ch);
+				if ((ch==123) && is_true_char(&tmpiter)) {/* { */
+					level--;
+					if (level==0) {
+						retval = TRUE;
+						break;
 					}
-				}else if(ch ==125) {/* } */
-					if (is_true_char(&tmpiter)) {
-						level --;
-						DEBUG_MSG("brace_finder: find true }. level down to %d\n", level);
-						if (level==0) {
-							retval = TRUE;
-							break; /* finish the loop */
-						}
+					DEBUG_MSG("[-%d]", level);
+				}else if((ch==125) && is_true_char(&tmpiter)) {/* } */
+					level ++;
+					DEBUG_MSG("[-%d]", level);
+				}else if (gtk_text_iter_ends_line(&tmpiter)) {
+					DEBUG_MSG("[line end]");
+					tmp2iter = tmpiter;
+					gtk_text_iter_set_line_offset(&tmp2iter,0);
+					/* if we are in a comment line
+					search from the begining of line to the end == tmpiter;
+					*/
+					ch = gtk_text_iter_get_char(&tmp2iter);
+					if ((ch ==37) || ( gtk_text_iter_forward_find_char(&tmp2iter, char_predicate, GINT_TO_POINTER(37), &tmpiter) && is_true_char(&tmp2iter) ) ) {
+						DEBUG_MSG("[found %%]");
+						tmpiter = tmp2iter;
 					}
 				}
 			}
 		}
 		/* finished */
 		if (retval) {
-			if (move_if_found) {
+			if (opt & MOVE_IF_FOUND) {
 				gtk_text_buffer_place_cursor (buffer, &tmpiter);
 			}
-			return TRUE;
+			return BR_RET_FOUND;
 		}
+		return BR_RET_NOT_FOUND;
 	}
 #ifdef DEBUG
 	else{
 		DEBUG_MSG("brace_finder: selection is not empy. does nothing\n");
 	}
 #endif
-	return FALSE;
+	return BR_RET_IN_SELECTION;
 }
