0cc6e625d8631ab0fe8e573140def863219e7a43
icy <kyanh@viettug.org>
2006-04-21 05:04:14 +0000

    revert to 2273

--- src/document.c
+++ src/document.c
@@ -1486,9 +1486,7 @@ static gchar *get_buffer_from_filename( Tbfwin *bfwin, gchar *filename, int *ret
 	GError *error = NULL;
 	gsize length;
 	gchar *ondiskencoding = get_filename_on_disk_encoding( filename );
-	g_print("get_buffer_from_filename: started\n");
 	result = g_file_get_contents( ondiskencoding, &buffer, &length, &error );
-	g_print("get_buffer_from_filename: done\n");
 	g_free( ondiskencoding );
 	if ( result == FALSE ) {
 		gchar * errmessage = g_strconcat( _( "Could not read file:\n" ), filename, NULL );
