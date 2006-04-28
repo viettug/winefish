77591b7db7eedcc6308523a3d9b8df523a91671c
icy <kyanh@viettug.org>
2006-04-28 14:57:28 +0000

    FUNC_ macros moved from snooper2.h

--- src/func_all.h
+++ src/func_all.h
@@ -8,4 +8,15 @@
 #include "func_comment.h"
 #include "func_indent.h"
 
+enum {
+	FUNC_VALUE_0 = 1<<0,
+	FUNC_VALUE_1 = 1<<1,
+	FUNC_ANY = 1<<2,
+	FUNC_TEXT_VIEW = 1<<3,
+	FUNC_FROM_SNOOPER = 1<<4,
+	FUNC_FROM_OTHER =1 <<5
+};
+
+#define FUNC_VALID_TYPE(type,widget) ( (type & FUNC_ANY) ||  ( (type & FUNC_TEXT_VIEW ) && GTK_IS_TEXT_VIEW(widget) ) )
+
 #endif /* __FUNC_ALL_H__ */
