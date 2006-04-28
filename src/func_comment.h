9764a51819a0d438e77284a31638a2e498bf6720
icy <kyanh@viettug.org>
2006-04-28 05:00:38 +0000

    drop (func_uncomment). added opt parameter...

--- src/func_comment.h
+++ src/func_comment.h
@@ -1,6 +1,6 @@
 #ifndef __FUNC_COMMENT_H__
 #define __FUNC_COMMENT_H__
 void menu_comment_cb( Tbfwin *bfwin, guint callback_action, GtkWidget *widget );
-gint func_comment(GtkWidget *widget, GdkEventKey *kevent, Tbfwin *bfwin);
-gint func_uncomment(GtkWidget *widget, GdkEventKey *kevent, Tbfwin *bfwin);
+gint func_comment(GtkWidget *widget, GdkEventKey *kevent, Tbfwin *bfwin, gint opt);
+/* gint func_uncomment(GtkWidget *widget, GdkEventKey *kevent, Tbfwin *bfwin, SNOOPER_MAX_INT opt); */
 #endif /* __FUNC_COMMENT_H__ */
