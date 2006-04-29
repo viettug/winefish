9e79fa2e8df91b3a921098c70ff86104a823200e
icy <kyanh@viettug.org>
2006-04-29 12:30:56 +0000

    added (func_delete_line)

--- src/func_delline.h
+++ src/func_delline.h
@@ -1,5 +1,6 @@
 #ifndef __FUNC_DELLINE_H__
 #define __FUNC_DELLINE_H__
 void menu_del_line_cb( Tbfwin *bfwin, guint callback_action, GtkWidget *widget );
+gint func_delete_line( GtkWidget *widget, GdkEventKey *kevent, Tbfwin *bfwin, gint opt );
 #endif /* __FUNC_DELLINE_H__ */
 
