067993ffea8f92574a8ebe4ce1ec76be638b08a0
icy <kyanh@viettug.org>
2006-04-29 13:02:24 +0000

    added func_delete_line (with full support: whole line, left line, right line)

--- src/func_delline.c
+++ src/func_delline.c
@@ -23,24 +23,46 @@
 
 #include "bluefish.h"
 
-/* kyanh, added */
-/* vers=0: remove from cursor to the end*/
-static void doc_del_line( Tdocument *doc, gboolean vers ) {
+/*
+ opt & FUNC_VALUE_0: delete the whole line
+ opt & FUNC_VALUE_1: delete to the end of line
+ opt & FUNC_VALUE_1: delete to the begining of line
+*/
+gint func_delete_line( GtkWidget *widget, GdkEventKey *kevent, Tbfwin *bfwin, gint opt ) {
+	Tdocument *doc = bfwin->current_document;
+	if (!doc) return 0;
+
 	GtkTextIter itstart, itend;
 	if ( !gtk_text_buffer_get_selection_bounds( doc->buffer, &itstart, &itend ) ) {
 		/* there is no selection, work on the current line */
 		GtkTextIter iter;
 		gtk_text_buffer_get_iter_at_mark( doc->buffer, &iter, gtk_text_buffer_get_insert( doc->buffer ) );
-		if ( vers ) {
+		/* delete the whole line */
+		if ( opt & FUNC_VALUE_0 ) {
 			gtk_text_iter_set_line_offset( &iter, 0 );
 			gtk_text_iter_forward_line( &itend );
+		/* delete to end of line */
+		} else if (opt & FUNC_VALUE_1 ) {
+			if ( gtk_text_iter_forward_line( &itend ) )
+				gtk_text_iter_backward_char(&itend);
+		/* delete to begin of line */
+		} else if (opt & FUNC_VALUE_2 ) {
+			gtk_text_iter_set_line_offset( &iter, 0 );
+		}
+		if (gtk_text_iter_compare(&iter, &itend) == -1) {
 			gtk_text_buffer_delete( doc->buffer, &iter, &itend );
+			return 1;
 		}
 	}
+	return 0;
 }
 
 void menu_del_line_cb( Tbfwin *bfwin, guint callback_action, GtkWidget *widget ) {
-	if ( bfwin->current_document ) {
-		doc_del_line( bfwin->current_document, ( callback_action == 1 ) );
+	gint opt = 0;
+	switch (callback_action) {
+	case 1: opt = FUNC_VALUE_1; break;
+	case 2: opt = FUNC_VALUE_2; break;
+	default: opt = FUNC_VALUE_0; break;
 	}
+	func_delete_line( widget, NULL, bfwin, opt );
 }
