1489b0bc2083652dfb5c6582968cefead74f07e3
icy <kyanh@viettug.org>
2006-05-01 04:24:06 +0000

    FUNC_VALUE_ = 10 (former 5) which is greater than any FUNC_VALUE_*

--- src/func_all.h
+++ src/func_all.h
@@ -14,12 +14,12 @@ enum {
 	FUNC_TEXT_VIEW = 1<<1,
 	FUNC_FROM_SNOOPER = 1<<2,
 	FUNC_FROM_OTHER =1 <<3,
-	FUNC_VALUE_0 = 1<<5,
-	FUNC_VALUE_1 = 1<<6,
-	FUNC_VALUE_2 = 1<<7,
+	FUNC_VALUE_0 = 1<<4,
+	FUNC_VALUE_1 = 1<<5,
+	FUNC_VALUE_2 = 1<<6,
 };
 
-#define FUNC_VALUE_  5
+#define FUNC_VALUE_  10
 
 #define FUNC_VALID_TYPE(type,widget) ( (type & FUNC_ANY) ||  ( (type & FUNC_TEXT_VIEW ) && GTK_IS_TEXT_VIEW(widget) ) )
 
