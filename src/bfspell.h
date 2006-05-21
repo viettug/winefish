2eb6f42b716326c1d606e31201c02f4058f248d1
icy <kyanh@viettug.org>
2006-05-21 06:36:01 +0000

    added func: _spell_check

--- src/bfspell.h
+++ src/bfspell.h
@@ -1,8 +1,9 @@
-/* $Id: bfspell.h,v 1.1.1.1 2005/06/29 11:03:16 kyanh Exp $ */
+/* $Id$ */
 /*
  * Modified for Winefish (C) 2005 Ky Anh <kyanh@o2.pl>
  */
 
 #ifdef HAVE_LIBASPELL
 void spell_check_cb(GtkWidget *widget, Tbfwin *bfwin);
+gint func_spell_check(GtkWidget *widget, GdkEventKey *kevent, Tbfwin *bfwin, gint opt);
 #endif /* HAVE_LIBASPELL */
