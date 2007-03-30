659d67a0b6f72c2275ec61794ce80d5d29d5ff09
icy <kyanh@viettug.org>
2007-03-30 17:40:59 +0000

    drop func_template_list, added (file_open_advanced_cb)

--- src/func_grep.h
+++ src/func_grep.h
@@ -6,11 +6,9 @@
 #ifdef EXTERNAL_GREP
 #ifdef EXTERNAL_FIND
 
-/* void file_open_advanced_cb( Tbfwin *bfwin, gboolean open_files); */
-gint func_grep( GtkWidget *widget, GdkEventKey *kevent, Tbfwin *bfwin, gint opt );
+void file_open_advanced_cb( Tbfwin *bfwin, gboolean open_files);
 void open_advanced_from_filebrowser( Tbfwin *bfwin, gchar *path );
-/* void template_rescan_cb(Tbfwin *bfwin); */
-gint func_template_list( GtkWidget *widget, GdkEventKey *kevent, Tbfwin *bfwin, gint opt );
+void template_rescan_cb(Tbfwin *bfwin);
 
 #endif /* EXTERNAL_GREP */
 #endif /* EXTERNAL_FIND */
